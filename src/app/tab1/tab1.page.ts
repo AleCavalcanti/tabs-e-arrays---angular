df7cb99622b86e7d26e3b34e242115e715467476
enzofigueiredo01 <enzrf01@gmail.com>
2026-05-26 11:59:40 -0300

    Codigo inicial pronto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,80 @@
+import { NavigationExtras, Router } from '@angular/router';
+
 import { Component } from '@angular/core';
+import { IFilme } from '../model/IFilme';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    public router: Router,
+    public alertController: AlertController,
+    public toastController: ToastController
+  ) {}
 
+  listaFilmes: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h50m',
+      classificacao: 9,
+      cartaz: 'https://themoviedb.org',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat',
+      favorito: false
+    },
+    {
+      nome: 'Vingadores: Ultimato (2019)',
+      lancamento: '25/04/2019 (BR)',
+      duracao: '3h01m',
+      classificacao: 6,
+      cartaz: 'https://themoviedb.org',
+      generos: ['Aventura', 'Ficção científica', 'Ação'],
+      pagina: '/avengers',
+      favorito: false
+    }
+  ];
+
+  exibirFilme(filme: IFilme){
+    const navigationExtras: NavigationExtras = { state: {paramFilme: filme}};
+    this.router.navigate(['filme-detalhe'], navigationExtras);
+  }
+
+  async exibirAlertaFavorito(filme: IFilme) {
+    const alert = await this.alertController.create({
+      header: 'Meus Favoritos',
+      message: 'Deseja realmente favoritar o filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            filme.favorito=false;
+          }
+        }, {
+          text: 'Sim, favoritar.',
+          handler: () => {
+            filme.favorito=true;
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos...',
+      duration: 2000,
+      color: 'success'
+    });
+    toast.present();
+  }
 }
